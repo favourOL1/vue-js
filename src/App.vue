916759074c560aef4e1a3c638ecece4a3d65f0b3
Favour <favourOL1@outlook.com>
2024-04-15 09:09:59 +0100

    my vue project

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import MyComponent from './components/MyComponent.vue';
+</script>
+
+<template>
+  <MyComponent msg="My first Vue Website" head="My name is Favour"/>
+  <HelloWorld msg="Vite + Vue"  head="hello"/>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/MyComponent.vue
@@ -0,0 +1,22 @@
+<script setup>
+import {ref} from 'vue'
+
+defineProps({
+    head:String,
+    msg: String,
+
+})
+
+</script>
+
+
+<template>
+
+    <h1>
+        {{ head }}
+    </h1>
+
+    <p>
+        {{ msg }}
+    </p>
+</template>
